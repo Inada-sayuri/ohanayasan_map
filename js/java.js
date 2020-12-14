81f38860018629b39c24fd565577d1eee48b5ab8
inada <inada.growup@gmail.com>
2020-12-14 15:04:31 +0900

    PC版グローバルメニュー実装

--- js/java.js
+++ js/java.js
@@ -1,26 +1,26 @@
-$(function(){
-$('.btn_wrap').on('click', function() {
+$(function() {
+    $('.btn_wrap').on('click', function() {
 
 
 
-$('.menu').toggleClass('over');
+        $('.menu').toggleClass('over');
 
-setTimeout(function(){
-	$('.menu li').addClass('lislide');
-},400);
+        setTimeout(function() {
+            $('.menu li').addClass('lislide');
+        }, 400);
 
-if($('.menu').hasClass('over')){
-	$('.menu li').removeClass('lislide');	
-}
-
-
-setTimeout(function(){
-$('.btn').toggleClass('btnout');
-},10);
-setTimeout(function(){
-$('.btn2').toggleClass('btnchange');
-},10);
+        if ($('.menu').hasClass('over')) {
+            $('.menu li').removeClass('lislide');
+        }
 
 
-});
+        setTimeout(function() {
+            $('.btn').toggleClass('btnout');
+        }, 10);
+        setTimeout(function() {
+            $('.btn2').toggleClass('btnchange');
+        }, 10);
+
+
+    });
 });
